00a22f2fdb627fd0e7b4b9d476eb086ceb8345c9
Naomi <naomi.l@payoda.com>
2024-05-27 00:35:50 +0530

    gh-pages fix 4

--- src/components/Pages/NotFound.js
+++ src/components/Pages/NotFound.js
@@ -1,6 +1,6 @@
 const NotFound = () => {
     return (
-        <div>
+        <div className="not-found">
             <h1>404 Not Found</h1>
             <p>The page you are looking for does not exist.</p>
         </div>
